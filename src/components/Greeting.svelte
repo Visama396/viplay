202938821c826ae9420ae04dc6c0e14b6e3be3d1
Victor <visama396@gmail.com>
2024-02-17 02:46:35 +0100

    First commit
    Playing music. Aside finished. Player persist between transitions.

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  let greeting = ""
+
+  if (currentHour < 12) {
+    greeting = "Good morning"
+  } else if (currentHour < 18) {
+    greeting = "Good afternoon"
+  } else {
+    greeting = "Good evening"
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
